6010849997fdb61b2957a54da96742b3c8102e35
Michael Pieschel <sixtyhectares@yahoo.com>
2018-11-08 18:32:20 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,6 @@ class UsersController < ApplicationController
     if !@user
       redirect_to controller: 'users', action: 'new'
     else
-      @user.save
       session[:user_id] = @user.id
       redirect_to controller: 'users', action: 'create'
     end
