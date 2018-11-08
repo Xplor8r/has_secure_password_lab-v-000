97b6e5d326495573865600899533e04ec78cdb23
Michael Pieschel <sixtyhectares@yahoo.com>
2018-11-08 18:34:35 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
 
   get 'users/create'
 
+  root 'application#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
